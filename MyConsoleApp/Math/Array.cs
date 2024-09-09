e9b44aa60489c244d028d6d7438dde9f04992e5d
Gucortez <guhcortez@icloud.com>
2024-09-08 22:37:05 -0600

    Arrays

--- MyConsoleApp/Math/Array.cs
+++ MyConsoleApp/Math/Array.cs
@@ -2,9 +2,51 @@ namespace MyConsoleApp.Math;
 
 public static class ArrayTest
 {   
-    public static int[] Arrays()
+    public static void Arrays()
     {
-        var numbers = new int[3];
-        return numbers;
+        int[] numbers = [3, 7, 9, 2, 14, 6];
+
+        // length
+        Console.WriteLine("Length: " + numbers.Length);
+
+        // IndexOf()
+        var index = Array.IndexOf(numbers, 9);
+        Console.WriteLine("Index of 9: " + index);
+
+        // Clear()
+        Array.Clear(numbers, 0, 2); // the first to itens are set to 0, or bool set to false or num in  str[]
+        Console.WriteLine("Effect of Clear()");
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        // Copy()
+        int[] another = new int[3];
+        Array.Copy(numbers, another, 3); // copies 3 elements of numbers array to another array
+
+        Console.WriteLine("Effect of Copy()");
+        foreach (var n in another)
+        {
+            Console.WriteLine(n);
+        }
+
+        // Sort()
+        Array.Sort(numbers); // organizes the array by value
+        Console.WriteLine("Effect of Sort()");
+        foreach (var n in numbers)
+        {
+            Console.WriteLine(n);   
+        }
+
+        // Reverse()
+        Array.Reverse(numbers); // reverses the array
+
+        Console.WriteLine("Effect of Reverse()");
+        foreach (var n in numbers)
+        {
+            Console.WriteLine(n);
+        };
     }
 }
--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,90 +1,88 @@
 ﻿using MyConsoleApp.Math;
-using MyConsoleApp.People;
-using MyConsoleApp.Classes;
-using MyConsoleApp.ConditionalStatements;
-using MyConsoleApp.IterationStatements;
+// using MyConsoleApp.People;
+// using MyConsoleApp.Classes;
+// using MyConsoleApp.ConditionalStatements;
+// using MyConsoleApp.IterationStatements;
 
 
-var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+// var john = new Person();
+//             john.FirstName = "John";
+//             john.LastName = "Smith";
+//             john.Introduce();
 
-var result = Calculator.Add(1, 2);
-Console.WriteLine(result);
+// var result = Calculator.Add(1, 2);
+// Console.WriteLine(result);
 
-// declaring arrays
-// int[] numbers = new int[3] {1, 2, 3};
-// values in {}
-string name = "Mosh";
-char firstChar = name[0];
-Console.WriteLine(firstChar);
+// // declaring arrays
+// // int[] numbers = new int[3] {1, 2, 3};
+// // values in {}
+// string name = "Mosh";
+// char firstChar = name[0];
+// Console.WriteLine(firstChar);
 
-var stringsLesson = StringTest.Names();
-Console.WriteLine(stringsLesson);
+// var stringsLesson = StringTest.Names();
+// Console.WriteLine(stringsLesson);
 
-var array = ArrayTest.Arrays();
-Console.WriteLine(string.Join(", ", array));
+// // instead of using multiple const you can use enum
+// /* 
+// const int RegularAirMail = 1,
+// const int RegisteredAirMail = 2,
+// const int Express = 3;
+// public enum Shipping method
+// {
+//     RegularAirMail = 1,
+//     RegisteredAirMail = 2,
+//     Express = 3;
+// }
+// */
+// var methodId = 3;
+// Console.WriteLine((EnumTest.ShippingMethod)methodId); // output = Express
 
-// instead of using multiple const you can use enum
-/* 
-const int RegularAirMail = 1,
-const int RegisteredAirMail = 2,
-const int Express = 3;
-public enum Shipping method
-{
-    RegularAirMail = 1,
-    RegisteredAirMail = 2,
-    Express = 3;
-}
-*/
-var methodId = 3;
-Console.WriteLine((EnumTest.ShippingMethod)methodId); // output = Express
+// var method = EnumTest.ShippingMethod.Express; // call the enum that you want
+// Console.WriteLine((int)method);
 
-var method = EnumTest.ShippingMethod.Express; // call the enum that you want
-Console.WriteLine((int)method);
+// Console.WriteLine(method.ToString()); // convert enum to string, output = Express
 
-Console.WriteLine(method.ToString()); // convert enum to string, output = Express
+// var methodName = "Express";
+// var shippingMethod = (EnumTest.ShippingMethod)Enum.Parse(typeof(EnumTest.ShippingMethod),methodName); // string to enum, parse and cast
+// Console.WriteLine(shippingMethod);
 
-var methodName = "Express";
-var shippingMethod = (EnumTest.ShippingMethod)Enum.Parse(typeof(EnumTest.ShippingMethod),methodName); // string to enum, parse and cast
-Console.WriteLine(shippingMethod);
+// var classText = ClassTests.ClassText();
+// Console.WriteLine(classText);
 
-var classText = ClassTests.ClassText();
-Console.WriteLine(classText);
+// var numb = 1;
+// Person.Increment(numb);
+// Console.WriteLine(numb); // the method takes a copy of the numb, making the original numb unchanged, because only the copy was altered. output = 1
 
-var numb = 1;
-Person.Increment(numb);
-Console.WriteLine(numb); // the method takes a copy of the numb, making the original numb unchanged, because only the copy was altered. output = 1
+// var person = new Person() {Age = 20};
+// Person.MakeOld(person);
+// Console.WriteLine(person.Age); // method takes the reference to the same memory location, so the object is altered. output = 30
 
-var person = new Person() {Age = 20};
-Person.MakeOld(person);
-Console.WriteLine(person.Age); // method takes the reference to the same memory location, so the object is altered. output = 30
+// /* 
+// Summary:
+// Value Types: Changes are made on a copy, so the original value remains unchanged (e.g., int, bool, char, struct).
+// Reference Types: Changes are made on the original object since the method operates on a reference to the same object (e.g., class, array, delegate).*/
 
-/* 
-Summary:
-Value Types: Changes are made on a copy, so the original value remains unchanged (e.g., int, bool, char, struct).
-Reference Types: Changes are made on the original object since the method operates on a reference to the same object (e.g., class, array, delegate).*/
+// IfElseLesson.IfElse(1);
+// IfElseLesson.Customer(true);
 
-IfElseLesson.IfElse(1);
-IfElseLesson.Customer(true);
+// Switch.switchSeasons((Season)2);
 
-Switch.switchSeasons((Season)2);
+// Exercise.NumberValidation();
+// int maxNumber = Exercise.MaxNumber();
+// Console.WriteLine(maxNumber);
+// Exercise.LandscapeOrPortrait();
+// Exercise.SpeedTest();
 
-Exercise.NumberValidation();
-int maxNumber = Exercise.MaxNumber();
-Console.WriteLine(maxNumber);
-Exercise.LandscapeOrPortrait();
-Exercise.SpeedTest();
+// ForLoops.ForLoopsTests(1);
+// ForeachLoops.ForeachLoopsTests("Jonh Smith");
+// ForeachLoops.ForeachLoopsArray([1,2,3,4,5,6,7,8]);
 
-ForLoops.ForLoopsTests(1);
-ForeachLoops.ForeachLoopsTests("Jonh Smith");
-ForeachLoops.ForeachLoopsArray([1,2,3,4,5,6,7,8]);
-
-WhileLoops.EcoProgram();
-RandomClass.RandomTests();
-InterationExercises.CountNumbers();
-InterationExercises.NumberFactorial();
-InterationExercises.OkExercise();
-InterationExercises.SeriesOfNumbers();
-InterationExercises.RandomNumber();
+// WhileLoops.EcoProgram();
+// RandomClass.RandomTests();
+// InterationExercises.CountNumbers();
+// InterationExercises.NumberFactorial();
+// InterationExercises.OkExercise();
+// InterationExercises.SeriesOfNumbers();
+// InterationExercises.RandomNumber();
+ArrayTest.Arrays();
